459adc17168a0b10a448c80350719230a6231e47
Clement <clement.bisaillon@hotmail.com>
2020-05-15 11:07:30 -0400

    cache busting

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
 <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
 <!-- Scripts -->
-<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="{{ mix('js/app.js') }}" defer></script>
 <script src="https://js.stripe.com/v3/"></script>
 
 <!-- Fonts -->
@@ -18,7 +18,7 @@
 <!-- Styles -->
 <link rel="stylesheet" href="https://unpkg.com/vue-material/dist/vue-material.min.css">
 <link rel="stylesheet" href="https://unpkg.com/vue-material/dist/theme/default.css">
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
 <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">
 
 <link rel="stylesheet" href="https://unpkg.com/vue-multiselect@2.1.0/dist/vue-multiselect.min.css">
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+    .version();
 
-mix.sass('node_modules/cropperjs/src/css/cropper.scss', 'public/css')
+mix.sass('node_modules/cropperjs/src/css/cropper.scss', 'public/css');
+
+
