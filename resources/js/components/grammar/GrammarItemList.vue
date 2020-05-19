3f2c725b426fbf11f649ff095725b48eb0aa9d65
Clement <clement.bisaillon@hotmail.com>
2020-05-19 16:09:02 -0400

    grammar no item empty state

--- resources/js/components/grammar/GrammarItemList.vue
+++ resources/js/components/grammar/GrammarItemList.vue
@@ -18,6 +18,14 @@
 
                     <slot></slot>
 
+                    <div v-if="items.data.length === 0">
+                        <md-empty-state
+                            md-icon="spellcheck"
+                            md-label="Grammar items"
+                            md-description="Sorry ! We haven't wrote the grammar items in this category yet.">
+                        </md-empty-state>
+                    </div>
+
                 </div>
             </div>
         </div>
