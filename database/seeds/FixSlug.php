c3ab5401763c61cbcd4c9f1191f9d4a95604b670
Clement <clement.bisaillon@hotmail.com>
2020-05-19 15:53:45 -0400

    test fix slug

--- database/seeds/FixSlug.php
+++ database/seeds/FixSlug.php
@@ -5,12 +5,12 @@ class FixSlug extends \Illuminate\Database\Seeder
 {
     public function run(){
         foreach (\App\Models\GrammarLearningPathItem::all() as $grammar){
-            $grammar->slug =  preg_replace('/[^A-Za-z0-9_\-]/', '', strtolower(str_replace(' ', "_", $grammar->title)));
+            $grammar->slug =  strtolower(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\-]/', '', $grammar->title))));
             $grammar->save();
         }
 
         foreach(\App\Models\Story::all() as $story){
-            $story->slug =  preg_replace('/[^A-Za-z0-9_\-]/', '', strtolower(str_replace(' ', "_", $story->title)));
+            $story->slug =  strtolower(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\-]/', '', $story->title))));
             $story->save();
         }
     }
