e0845c65dd8f3562d2d743008b49443619005faf
Clement <clement.bisaillon@hotmail.com>
2020-05-19 15:57:49 -0400

    test fix slug

--- database/seeds/FixSlug.php
+++ database/seeds/FixSlug.php
@@ -5,18 +5,12 @@ class FixSlug extends \Illuminate\Database\Seeder
 {
     public function run(){
         foreach (\App\Models\GrammarLearningPathItem::all() as $grammar){
-            $grammar->slug =  strtolower(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\s\-]/', '', $grammar->title))));
+            $grammar->slug =  strtolower(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\s]/', '', $grammar->title))));
             $grammar->save();
         }
 
         foreach(\App\Models\Story::all() as $story){
-
-            error_log(preg_replace('/[^A-Za-z0-9_\s\-]/', '', $story->title));
-            error_log(trim(preg_replace('/[^A-Za-z0-9_\s\-]/', '', $story->title)));
-            error_log(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\s\-]/', '', $story->title))));
-            error_log();
-
-            $story->slug =  strtolower(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\s\-]/', '', $story->title))));
+            $story->slug =  strtolower(str_replace(' ', "_", trim(preg_replace('/[^A-Za-z0-9_\s]/', '', $story->title))));
             $story->save();
         }
     }
