a4fce68a2383faf6ae52a29ef8b0bec066c1f268
Clement <clement.bisaillon@hotmail.com>
2020-05-28 13:45:41 -0400

    fix state

--- resources/views/app/account/settings/teacher/getPaid.blade.php
+++ resources/views/app/account/settings/teacher/getPaid.blade.php
@@ -15,7 +15,7 @@
 
             <md-button
                 class="md-raised md-primary"
-                href="https://connect.stripe.com/express/oauth/authorize?client_id=ca_H6v5WaI8qXpgzmqjOWtRg0YmkElxjEGf&state={{$user->stripe_state}}&suggested_capabilities[]=transfers&stripe_user[email]={{$user->email}}&stripe_user[country]=CA&stripe_user[first_name]={{$user->firstname}}&stripe_user[first_name]={{$user->lastname}}&stripe_user[currency]=cad">Setup stripe account</md-button>
+                href="https://connect.stripe.com/express/oauth/authorize?client_id=ca_H6v5WaI8qXpgzmqjOWtRg0YmkElxjEGf&state={{$user->info->stripe_state}}&suggested_capabilities[]=transfers&stripe_user[email]={{$user->email}}&stripe_user[country]=CA&stripe_user[first_name]={{$user->firstname}}&stripe_user[first_name]={{$user->lastname}}&stripe_user[currency]=cad">Setup stripe account</md-button>
         @else
             <p>Your stripe account has been set up. You can now do video lessons and get paid.</p>
         @endif
