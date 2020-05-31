add13752d4a7c917414f4f0a021811c6392f2aa9
Clement <clement.bisaillon@hotmail.com>
2020-05-31 17:07:45 -0400

    login api route

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', 'api\AuthController@login');
-Route::post('register', 'api\AuthController@register');
+Route::post('login', 'Api\AuthController@login');
+Route::post('register', 'Api\AuthController@register');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
